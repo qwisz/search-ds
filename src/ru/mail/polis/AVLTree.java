841d51b744da4cde477961777389fea71cc219b9
qwisz <barybin.anton@gmail.com>
2016-12-20 00:29:30 +0300

    add AVLTree

--- src/ru/mail/polis/AVLTree.java
+++ src/ru/mail/polis/AVLTree.java
@@ -2,11 +2,40 @@ package ru.mail.polis;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 //TODO: write code here
 public class AVLTree<E extends Comparable<E>> implements ISortedSet<E> {
 
-//    private Node root;
+    class Node {
+
+        Node(E value, Node parent) {
+            this.value = value;
+            this.parent = parent;
+        }
+
+        E value;
+        Node left;
+        Node right;
+        Node parent;
+        int balance;
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("N{");
+            sb.append("d=").append(value);
+            if (left != null) {
+                sb.append(", l=").append(left);
+            }
+            if (right != null) {
+                sb.append(", r=").append(right);
+            }
+            sb.append('}');
+            return sb.toString();
+        }
+    }
+
+    private Node root;
     private int size;
     private final Comparator<E> comparator;
 
@@ -20,12 +49,26 @@ public class AVLTree<E extends Comparable<E>> implements ISortedSet<E> {
 
     @Override
     public E first() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("set is empty, no first element");
+        }
+        Node curr = root;
+        while(curr.left != null){
+            curr = curr.left;
+        }
+        return curr.value;
     }
 
     @Override
     public E last() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("set is empty, no first element");
+        }
+        Node curr = root;
+        while(curr.right != null){
+            curr = curr.right;
+        }
+        return curr.value;
     }
 
     @Override
@@ -35,30 +78,210 @@ public class AVLTree<E extends Comparable<E>> implements ISortedSet<E> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return root == null;
     }
 
     @Override
     public boolean contains(E value) {
+        if (value == null) {
+            throw new NullPointerException("value is null");
+        }
+        if (root != null) {
+            Node curr = root;
+            while (curr != null) {
+                int cmp = compare(curr.value, value);
+                if (cmp == 0)
+                    return true;
+                else if (cmp < 0)
+                    curr = curr.right;
+                else
+                    curr = curr.left;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean add(E value) {
-        return false;
+        if (root == null)
+            root = new Node(value, null);
+        else {
+            Node n = root;
+            Node parent;
+            while (true) {
+                int cmp = compare(n.value, value);
+                if (cmp == 0)
+                    return false;
+
+                parent = n;
+
+                boolean goLeft = cmp > 0;
+                n = goLeft ? n.left : n.right;
+
+                if (n == null) {
+                    if (goLeft) {
+                        parent.left = new Node(value, parent);
+                    } else {
+                        parent.right = new Node(value, parent);
+                    }
+                    rebalance(parent);
+                    break;
+                }
+            }
+        }
+        size++;
+        return true;
+    }
+
+    private int height(Node n) {
+        if (n == null)
+            return -1;
+        return 1 + Math.max(height(n.left), height(n.right));
+    }
+
+    public int getHeight(){
+        return height(root);
+    }
+
+    private void setBalance(Node... nodes){
+        for (Node n : nodes) {
+            n.balance = height(n.right) - height(n.left);
+        }
+    }
+
+    private Node rotateLeft(Node a){
+        Node b = a.right;
+        b.parent = a.parent;
+        a.right = b.left;
+        if (a.right != null) {
+            a.right.parent = a;
+        }
+        b.left = a;
+        a.parent = b;
+        if (b.parent != null) {
+            if (b.parent.right == a) {
+                b.parent.right = b;
+            } else {
+                b.parent.left = b;
+            }
+        }
+        setBalance(a, b);
+        return b;
+    }
+
+    private Node rotateRight(Node a) {
+
+        Node b = a.left;
+        b.parent = a.parent;
+
+        a.left = b.right;
+
+        if (a.left != null)
+            a.left.parent = a;
+
+        b.right = a;
+        a.parent = b;
+
+        if (b.parent != null) {
+            if (b.parent.right == a) {
+                b.parent.right = b;
+            } else {
+                b.parent.left = b;
+            }
+        }
+
+        setBalance(a, b);
+
+        return b;
+    }
+
+    private Node rotateLeftThenRight(Node n) {
+        n.left = rotateLeft(n.left);
+        return rotateRight(n);
+    }
+
+    private Node rotateRightThenLeft(Node n) {
+        n.right = rotateRight(n.right);
+        return rotateLeft(n);
+    }
+
+    private void rebalance(Node n) {
+        setBalance(n);
+
+        if (n.balance == -2) {
+            if (height(n.left.left) >= height(n.left.right))
+                n = rotateRight(n);
+            else
+                n = rotateLeftThenRight(n);
+
+        } else if (n.balance == 2) {
+            if (height(n.right.right) >= height(n.right.left))
+                n = rotateLeft(n);
+            else
+                n = rotateRightThenLeft(n);
+        }
+
+        if (n.parent != null) {
+            rebalance(n.parent);
+        } else {
+            root = n;
+        }
     }
 
     @Override
     public boolean remove(E value) {
+        if (root == null)
+            return false;
+        Node n = root;
+        Node parent = root;
+        Node delNode = null;
+        Node child = root;
+
+        while (child != null) {
+            parent = n;
+            n = child;
+            int cmp = compare(value, n.value);
+            child = cmp >= 0 ? n.right : n.left;
+            if (cmp == 0)
+                delNode = n;
+        }
+
+        if (delNode != null) {
+            delNode.value = n.value;
+
+            child = n.left != null ? n.left : n.right;
+
+            if (compare(root.value, value) == 0) {
+                root = child;
+            } else {
+                if (parent.left == n) {
+                    parent.left = child;
+                } else {
+                    parent.right = child;
+                }
+                rebalance(parent);
+            }
+            size--;
+            return true;
+        }
         return false;
     }
 
     private int compare(E v1, E v2) {
         return comparator == null ? v1.compareTo(v2) : comparator.compare(v1, v2);
+    }
+
+
+    public static void main(String[] args) {
+        AVLTree<Integer> tree = new AVLTree<>();
+        for (int i = 0; i < 8; i++) {
+            tree.add(i);
+            System.out.println("add " + i + ", height = " + tree.getHeight());
+        }
     }
 }
